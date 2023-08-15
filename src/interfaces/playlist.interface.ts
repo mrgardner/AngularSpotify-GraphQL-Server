0ffda83c518ede4595b71eab62fc317028b7ee68
Matt Gardner <matthew.gardner.mrg@gmail.com>
2023-08-15 17:10:44 -0400

    Fixed playlist interface and schema

--- src/interfaces/playlist.interface.ts
+++ src/interfaces/playlist.interface.ts
@@ -1,138 +1,178 @@
-import { Followers } from "./user.interface"
+import { Followers } from "./user.interface";
 
 export interface TrackResponse {
-  href: string
-  items: [TrackInfo]
-  limit: number
-  next: string
-  offset: number
-  previous: string
-  total: number
+  href: string;
+  items: TrackInfo[];
+  limit: number;
+  next: string;
+  offset: number;
+  previous: string;
+  total: number;
 }
 
 export interface TrackInfo {
-  added_at: string
-  added_by: AddedBy
-  is_local: boolean
-  track: Track
-  video_thumbnail: VideoThumbNail
+  added_at: string;
+  added_by: AddedBy;
+  is_local: boolean;
+  track: Track;
+  video_thumbnail: VideoThumbNail;
 }
 
-export interface AddedBy {
-  external_urls: ExternalUrls
-  href: string
-  id: string
-  type: string
-  uri: string
+export interface PlaylistResponse {
+  href: string;
+  items: [Playlist];
+  limit: number;
+  next: string;
+  offset: number;
+  previous: string;
+  total: number;
+}
+
+export interface Playlist {
+  collaborative: boolean;
+  description: string;
+  external_urls: ExternalUrls;
+  followers: Followers;
+  href: string;
+  id: string;
+  images: Image[];
+  name: string;
+  owner: Owner;
+  public: boolean;
+  snapshot_id: string;
+  tracks: Tracks;
+  type: string;
+  uri: string;
+}
+
+export interface Tracks {
+  href: string;
+  limit: number;
+  next: string;
+  offset: number;
+  previous: string;
+  total: number;
+  items: TrackItems[];
+}
+
+export interface TrackItems {
+  added_at: string;
+  added_by: AddedBy;
+  is_local: boolean;
+  track: Track
 }
 
 export interface Track {
-  album: Album
-  artists: [Artist]
-  available_markets: [string]
-  disc_number: number
-  duration_ms: number
-  episode: boolean
-  explicit: boolean
-  external_ids: ExternalIds
-  external_urls: ExternalUrls
-  href: string
-  id: string
+  album: Album;
+  artists: Artist[];
+  available_markets: string[];
+  disc_number: number;
+  duration_ms: number;
+  explicit: boolean;
+  external_ids: ExternalIds;
+  external_urls: ExternalUrls;
+  href: string;
+  id: string;
+  is_playable: boolean;
+  linked_from: object;
+  restrictions: Restrictions;
+  name: string;
+  popularity: number;
+  preview_url: string;
+  track_number: number;
+  type: string;
+  uri: string;
   is_local: boolean
-  name: string
-  popularity: number
-  preview_url: string
-  track: boolean
-  track_number: number
-  type: string
-  uri: string
 }
 
 export interface Album {
-  album_type: string
-  artists: [Artist]
-  available_markets: [string]
-  external_urls: ExternalUrls
-  href: string
-  id: string
-  images: [Image]
-  name: string
-  release_date: string
-  release_date_precision: string
-  total_tracks: number
-  type: string
+  album_type: string;
+  total_tracks: number;
+  available_markets: string[];
+  external_urls: ExternalUrls;
+  href: string;
+  id: string;
+  images: Image[];
+  name: string;
+  release_date: string;
+  release_date_precision: string;
+  restrictions: Restrictions;
+  type: string;
+  uri: string;
+  copyrights: Copyright[];
+  external_ids: ExternalIds;
+  genres: string[];
+  label: string;
+  popularity: number;
+  album_group: string;
+  artists: AlbumArtist[];
+}
+
+export interface AlbumArtist {
+  external_urls: ExternalUrls;
+  href: string;
+  id: string;
+  name: string;
+  type: string;
   uri: string
 }
 
 export interface Artist {
-  external_urls: ExternalUrls
-  href: string
-  id: string
-  name: string
-  type: string
+  external_urls: ExternalUrls;
+  followers: Followers;
+  genres: string[];
+  href: string;
+  id: string;
+  images: Image[];
+  name: string;
+  popularity: number;
+  type: string;
   uri: string
-}
-
-export interface ExternalIds {
-  isrc: string
-}
-
-export interface VideoThumbNail {
-  url: string
-}
-
-export interface PlaylistResponse {
-  href: string
-  items: [Playlist]
-  limit: number
-  next: string
-  offset: number
-  previous: string
-  total: number
-}
-
-export interface ExternalUrls {
-  spotify: string
-}
-
-export interface Image {
-  height: number
-  url: string
-  width: number
 }
 
 export interface Owner {
+  external_urls: ExternalUrls;
+  followers: Followers;
+  href: string;
+  id: string;
+  type: string;
+  uri: string;
   display_name: string
-  external_urls: ExternalUrls
-  href: string
-  id: string
-  type: string
-  uri: string
 }
 
-export interface Tracks {
-  href: string
-  items: [Track]
-  limit: number
-  next: string
-  offset: number
-  previous: string
-  total: number
+export interface AddedBy {
+  external_urls: ExternalUrls;
+  followers: Followers;
+  href: string;
+  id: string;
+  type: string;
+  uri: string
+}
+export interface Image {
+  height: number;
+  url: string;
+  width: number;
 }
 
-export interface Playlist {
-  collaborative: boolean
-  external_urls: ExternalUrls
-  href: string
-  id: string
-  images: [Image]
-  name: string
-  owner: Owner
-  primary_color: string
-  public: boolean
-  snapshot_id: string
-  tracks: Tracks
+export interface Copyright {
+  text: string;
   type: string
-  uri: string
-  followers: Followers
+}
+
+export interface ExternalUrls {
+  spotify: string;
+}
+
+export interface Restrictions {
+  reason: string
+}
+
+export interface ExternalIds {
+  isrc: string;
+  ean: string;
+  upc: string
+}
+
+export interface VideoThumbNail {
+  url: string;
 }
--- src/schema/playlist.schema.ts
+++ src/schema/playlist.schema.ts
@@ -1,10 +1,12 @@
 export const playlistTypes = `#graphql
+  scalar Object
+
   type Query {
     playlists(offset: Int!): PlaylistResponse,
     playlist(playlistID: String!): Playlist,
     playlistTracks(offset: Int, playlistID: String!, limit: Int): TrackResponse
   }
-  
+
   type TrackResponse {
     href: String
     items: [TrackInfo]
@@ -23,69 +25,6 @@ export const playlistTypes = `#graphql
     video_thumbnail: VideoThumbNail
   }
 
-  type AddedBy {
-    external_urls: ExternalUrls
-    href: String
-    id: String
-    type: String
-    uri: String
-  }
-  
-  type Track {
-    album: Album
-    artists: [Artist]
-    available_markets: [String]
-    disc_number: Int
-    duration_ms: Int
-    episode: Boolean
-    explicit: Boolean
-    external_ids: ExternalIds
-    external_urls: ExternalUrls
-    href: String
-    id: String
-    is_local: Boolean
-    name: String
-    popularity: Int
-    preview_url: String
-    track: Boolean
-    track_number: Int
-    type: String
-    uri: String
-  }
-
-  type Album {
-    album_type: String
-    artists: [Artist]
-    available_markets: [String]
-    external_urls: ExternalUrls
-    href: String
-    id: String
-    images: [Image]
-    name: String
-    release_date: String
-    release_date_precision: String
-    total_tracks: Int
-    type: String
-    uri: String
-  }
-
-  type Artist {
-    external_urls: ExternalUrls
-    href: String
-    id: String
-    name: String
-    type: String
-    uri: String
-  }
-
-  type ExternalIds {
-    isrc: String
-  }
-
-  type VideoThumbNail {
-    url: String
-  }
-
   type PlaylistResponse {
     href: String
     items: [Playlist]
@@ -96,49 +35,152 @@ export const playlistTypes = `#graphql
     total: Int
   }
 
-  type ExternalUrls {
-    spotify: String
-  }
-
-  type Image {
-    height: Int
-    url: String
-    width: Int
-  }
-
-  type Owner {
-    display_name: String
+    type Playlist {
+    collaborative: Boolean
+    description: String
     external_urls: ExternalUrls
+    followers: Followers
     href: String
     id: String
+    images: [Image]
+    name: String
+    owner: Owner
+    public: Boolean
+    snapshot_id: String
+    tracks: Tracks
     type: String
     uri: String
   }
 
   type Tracks {
     href: String
-    items: [Track]
     limit: Int
     next: String
     offset: Int
     previous: String
     total: Int
+    items: [TrackItems]
   }
 
-  type Playlist {
-    collaborative: Boolean
+  type TrackItems {
+    added_at: String
+    added_by: AddedBy
+    is_local: Boolean
+    track: Track
+  }
+
+  type Track {
+    album: Album
+    artists: [Artist]
+    available_markets: [String]
+    disc_Int: Int
+    duration_ms: Int
+    explicit: Boolean
+    external_ids: ExternalIds
+    external_urls: ExternalUrls
+    href: String
+    id: String
+    is_playable: Boolean
+    linked_from: Object
+    restrictions: Restrictions
+    name: String
+    popularity: Int
+    preview_url: String
+    track_number: Int
+    type: String
+    uri: String
+    is_local: Boolean
+  }
+
+  type Album {
+    album_type: String
+    total_tracks: Int
+    available_markets: [String]
     external_urls: ExternalUrls
     href: String
     id: String
     images: [Image]
     name: String
-    owner: Owner
-    primary_color: String
-    public: Boolean
-    snapshot_id: String
-    tracks: Tracks
+    release_date: String
+    release_date_precision: String
+    restrictions: Restrictions
     type: String
     uri: String
-    followers: Followers
+    copyrights: [Copyright]
+    external_ids: ExternalIds
+    genres: [String]
+    label: String
+    popularity: Int
+    album_group: String
+    artists: [AlbumArtist]
   }
-`;
+
+  type AlbumArtist {
+    external_urls: ExternalUrls
+    href: String
+    id: String
+    name: String
+    type: String
+    uri: String
+  }
+
+  type Artist {
+    external_urls: ExternalUrls
+    followers: Followers
+    genres: [String]
+    href: String
+    id: String
+    images: [Image]
+    name: String
+    popularity: Int
+    type: String
+    uri: String
+  }
+
+  type Owner {
+    external_urls: ExternalUrls
+    followers: Followers
+    href: String
+    id: String
+    type: String
+    uri: String
+    display_name: String
+  }
+
+  type AddedBy {
+    external_urls: ExternalUrls
+    followers: Followers
+    href: String
+    id: String
+    type: String
+    uri: String
+  }
+  type Image {
+    height: Int
+    url: String
+    width: Int
+  }
+
+  type Copyright {
+    text: String
+    type: String
+  }
+
+  type ExternalUrls {
+    spotify: String
+  }
+
+  type Restrictions {
+    reason: String
+  }
+
+  type ExternalIds {
+    isrc: String
+    ean: String
+    upc: String
+  }
+
+  type VideoThumbNail {
+    url: String
+  }
+`
